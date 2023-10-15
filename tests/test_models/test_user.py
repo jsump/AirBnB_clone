7f667b1f8c73670d340cfe62448ca18badd5d53b
jsump <libbyn79@gmail.com>
2023-10-15 16:40:02 -0700

    User class update

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,26 +1,38 @@
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
-    def test_user_attributes(self):
-        user = User()
-        self.assertEqual(user.email, "")
-        self.assertEqual(user.password, "")
-        self.assertEqual(user.first_name, "")
-        self.assertEqual(user.last_name, "")
+    def setUp(self):
+        self.user = User()
 
-    def test_user_attributes_update(self):
-        user = User()
-        user.email = "test@example.com"
-        user.password = "password123abc"
-        user.first_name = "Jane"
-        user.last_name = "Smith"
+    def test_email_initial_value(self):
+        self.assertEqual(self.user.email, "")
 
-        self.assertEqual(user.email, "test@example.com")
-        self.assertEqual(user.password, "password123abc")
-        self.assertEqual(user.first_name, "Jane")
-        self.assertEqual(user.last_name, "Smith")
+    def test_password_initial_value(self):
+        self.assertEqual(self.user.password, "")
+
+    def test_first_name_initial_value(self):
+        self.assertEqual(self.user.first_name, "")
+    
+    def test_last_name_initial_value(self):
+        self.assertEqual(self.user.last_name, "")
+
+    def test_inheritance_from_base_model(self):
+        self.assertTrue(issubclass(User, BaseModel))
+       
+    def test_user_attributes_inherited_from_base_model(self):
+        self.assertTrue(hasattr(self.user, "id"))
+        self.assertTrue(hasattr(self.user, "created_at"))
+        self.assertTrue(hasattr(self.user, "updated_at"))
+
+
+    def test_user_attributes_are_strings(self):
+        self.assertIsInstance(self.user.email, str)
+        self.assertIsInstance(self.user.password, str)
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
 
 
 if __name__ == "__main__":
